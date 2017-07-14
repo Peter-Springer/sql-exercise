28bc7760d416009e6e48d39db007af92b768c79e
Jeff Mayer and Peter Springer <localadmin@localadmins-iMac.local>
2017-07-14 16:25:34 -0500

    update seed and migration

---
+++ migration.sql
@@ -0,0 +1,12 @@
+DROP TABLE movies;
+
+CREATE TABLE movies (
+  id INT SERIAL DEFAULT VALUE,
+  title TEXT,
+  duration INT,
+  rating VARCHAR(10),
+  genre TEXT,
+  is_3d BOOLEAN NOT NULL,
+  released_at TIMESTAMP,
+  score INT(3, 1)
+);
--- seed.sql
+++ seed.sql
@@ -1,1 +1,24 @@
-Hi there
+DELETE FROM movies;
+INSERT INTO movies (title, duration, rating, genre, is_3d, released_at, score) VALUES ('Frozen', 102, 'PG', 'Animation', TRUE, '2013-11-27 00:00:00', 7.6);
+INSERT INTO movies (title, duration, rating, genre, is_3d, released_at, score) VALUES ('X-Men: Apocalypse', 144, 'PG-13', 'Action', TRUE, '2016-05-27 00:00:00', 7.4);
+INSERT INTO movies (title, duration, rating, genre, is_3d, released_at, score) VALUES ('The Princess Bride', 98, 'PG', 'Adventure', FALSE, '1987-10-09 00:00:00', 8.1);
+INSERT INTO movies (title, duration, rating, genre, is_3d, released_at, score) VALUES ('Pulp Fiction', 154, 'R', 'Crime', FALSE, '1994-10-14 00:00:00', 8.9);
+
+-- SELECT id, title, score
+-- from movies where score = (select MIN(score) from movies);
+
+-- SELECT id, title, duration 
+-- from movies
+-- WHERE title = "X-Men: Apocalypse" OR title="The Princess Bride";
+
+-- SELECT id, title, released_at from movies order by released_at;
+
+-- SELECT id, title, genre, score
+-- from movies
+-- where rating = "PG" and score BETWEEN 7.5 AND 8.5;
+
+UPDATE movies 
+SET score = score + 0.5
+where genre = "Crime";
+
+SELECT * FROM movies;
